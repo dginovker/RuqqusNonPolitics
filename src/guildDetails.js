81c0f60185d3bc8eeb80385e7e624413230e654f
dginovker <dcress01@uoguelph.ca>
2020-10-05 12:55:04 -0400

    It works!

--- src/guildDetails.js
+++ src/guildDetails.js
@@ -1,18 +1,35 @@
 import { readFileSync } from "fs";
 import { client } from "./index.js";
 
-function getTopPost(guildName) {
-  client.guilds.get(guildName).fetchPosts("new", 24, 1).then(res => {
-    console.log(res);
+async function getTopPost(guildName) {
+  return client.guilds.get(guildName).fetchPosts("top", 2, 1).catch(async e => {
+    console.log("Failed to get " + guildName + "... Retrying");
+    return await getTopPost(guildName);
   });
 }
 
-export default function guildDetails() {
+export default async function guildDetails() {
+  let guilds = [];
   readFileSync("guildlist.txt", "utf-8")
     .split(/\r?\n/)
     .forEach((line) => {
       if (!line.startsWith("#")) {
-        getTopPost(line.split("+")[1]);
+        guilds.push(line.split("+")[1]);
       }
     });
+
+  let toplist = [];
+
+  for (let i = 0; i < guilds.length; i++) {
+    let topTwo = await getTopPost(guilds[i]);
+    toplist.push(...topTwo);
+  }
+
+  toplist = toplist.sort((a, b) => {
+    return b.votes.score - a.votes.score;
+  });
+
+  toplist.forEach(topPost => {
+    console.log("Votes: " + topPost.votes.score + " --- Url: " + topPost.full_link);
+  });
 }
