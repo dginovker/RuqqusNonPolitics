3c0c4bd13f373f7d0134fdf5ea996122acdb7293
dginovker <dcress01@uoguelph.ca>
2020-10-31 00:01:21 -0400

    Cool new auth method uwu

--- src/index.js
+++ src/index.js
@@ -3,12 +3,7 @@ import guildDetails from "./guildDetails.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-// Create a new Client instance
-export const client = new Ruqqus.Client({
-  id: process.env.ID,
-  token: process.env.CLIENT_SECRET,
-  code: process.env.FUNNY_AUTH.split("=")[1].split("&")[0],
-});
+export const client = new Ruqqus.Client();
 
 // Login event
 client.on("login", async () => {
